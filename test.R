c7f3280b68a185baf6cf0850832bc847b7d8c092
nsaripalli <nithinsaripalli@gmail.com>
2020-06-09 13:31:12 -0500

    init test code

---
+++ test.R
@@ -0,0 +1,12 @@
+library(promisedyntracer)
+
+dyntrace_promises({
+                    # Recursive function to find factorial
+                    recursive.factorial <- function(x) {
+                      if (x == 0)    return (1)
+                      else           return (x * recursive.factorial(x-1))
+                    }
+
+                    recursive.factorial(5)
+}
+, './znalysis')
